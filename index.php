7b8ef58a14e08df53fbc68fe602b5fd675bd16cf
Olarinre <33872496+Olarinre@users.noreply.github.com>
2020-06-11 22:35:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+
+<! doctype html>
+<html>
+<head>
+<style type="text/css">
+	<?php include "mydemocss.css"
+	?>
+</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> This project will be added to my portfolio</title><!-- this might eventually be my website -->
+</head>
+<body> 
+	<header>
+		<img src ="mydemo.jpg" alt ="mydemo image"/>
+		<nav>
+			<ul>
+				<a href = "index.php" ><li>Home</li></a>
+				<a href = " portfolio.html " ><li>My portfolio</li></a>
+				<a href = " contact.html" ><li>contact</li></a>
+				<a href = ""><li>github/linkedin</li></a>
+			</ul>
+		</nav>
+	</header>
+	<div class ="maincontent">
+		
+		<article class = "mainarticle">
+			<header> <h2>Hi, </h2>
+				<img src = "dp.jpg" alt ="my display picture"/>
+
+			</header>
+
+			<footer class ="articleinfo">
+				<!--<p class ="articleinfofooter"> basic introduction </p>-->
+			</footer>
+			<content>
+			<p>My name is Ayobami, i am a student of the university of ibadan from the department
+				of computer science. i am a web developer (full stack), python programmer and Ai enthusiast.
+				So this is actually the very first project i wil be taking very seriously as a full stack developer.
+				i will be using htnl5, css3 + bootstrap and javascript for front-end design, while php will soley be used for the backend. ok so, in the nearest future, i will be converting this to my personal webpage. i really do hope this works out
+				as i intended. </p>	
+			</content>
+			
+		</article>
+
+
+	</div>
+	<aside>
+		<h2>Recent Projects</h2>
+		<p>	This web page will be a dynamic and responsive webpage.
+		<br/> ok, so i will explain what i put together to acmplish this.
+		<ol>
+		<li>html: making the structure of this webpage</li>
+		<li>css: will be used to set the page layout, style the basic html elements</li>
+		<li>javascript: will be used to handle interactivity on the webpage, make it dynamic</li>
+		<li>php: in conjunction with mysqli will be used to handle the backend, database, process forms</li>
+		 </p>	
+		
+	</aside>
+
+		
+
+
+
+
+
+
+
+
+
+
+<footer class ="mainfooter"> <p> copywright &copy 2020 | Olarinre Ayobami.</p> </footer>
+</body>
+</html>
