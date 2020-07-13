d40e11df4377af9e56cbd8995dfac6a77a032a16
Olarinre <33872496+Olarinre@users.noreply.github.com>
2020-07-13 11:14:10 +0100

    Add files via upload

--- jsinteract.js
+++ jsinteract.js
@@ -1,14 +1,11 @@
 // menu bar toggle finctionality
 const menubtn = document.querySelector(".sidebar-toggle");
 const closebtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector('.menuhide');
+const sidebar = document.querySelector(".menuhide");
 
 
 menubtn.addEventListener('click', function(){
-    if (sidebar.classList.contains("menuhide")){
-        sidebar.classList.remove("menuhide");
-    }
-
+    sidebar.classList.toggle("showmenu")
 
 });
 
